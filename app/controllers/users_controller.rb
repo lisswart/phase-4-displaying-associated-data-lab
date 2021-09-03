52aac3b62915e5117f1e4cf9cafae51385a92b81
lisswart <arnoudlisa@gmail.com>
2021-09-03 19:21:16 -0400

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users, include: :items
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, include: :items
+  end
 end
